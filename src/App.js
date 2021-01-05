9362467d5aea234baa59229335b8a5425bfb67c0
kiprotichdominic <kiprotichdominic102@gmail.com>
2021-01-05 15:12:08 +0300

    finish up on banner

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import {
   Route
 } from "react-router-dom";
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
-          <Route path="/checkout">Checkout</Route>
+          <Route path="/checkout">
+          <Header/>
+            Checkout</Route>
           <Route path="/login">Login</Route>
           <Route path="/">
             <Header/>
-            Home
+            <Home/>
             </Route>          
         </Switch>
       </div>
