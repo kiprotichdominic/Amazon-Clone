52b388bee7fac8d0c640e272dc741e69132c9ab0
kiprotichdominic <kiprotichdominic102@gmail.com>
2021-01-09 10:25:33 +0300

    finish product component

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,65 @@
-import React from 'react'
-import "./Home.css"
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
 
 function Home() {
-    return (
-        <div className="home">
-            <img className="home__image" src="https://images-na.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt=""/>
-        </div>
-    )
+  return (
+    <div className='home'>
+      <img
+        className='home__image'
+        src='https://images-na.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg'
+        alt=''
+      />
+      <div className='home__row'>
+        <Product
+          id='1323'
+          title='The Lean Start'
+          price={11.96}
+          rating={3}
+          image='https://images-na.ssl-images-amazon.com/images/G/01/amazonglobal/images/email/asins/DURM-2447E617EC5C463L._V533746472_.jpg'
+        />
+        <Product
+          id='1323'
+          title='The Lean Start'
+          price={11.96}
+          rating={3}
+          image='https://images-na.ssl-images-amazon.com/images/G/01/amazonglobal/images/email/asins/DURM-2447E617EC5C463L._V533746472_.jpg'
+        />
+      </div>
+      <div className='home__row'>
+        <Product
+          id='1323'
+          title='The Lean Start'
+          price={11.96}
+          rating={3}
+          image='https://images-na.ssl-images-amazon.com/images/G/01/amazonglobal/images/email/asins/DURM-2447E617EC5C463L._V533746472_.jpg'
+        />
+        <Product
+          id='1323'
+          title='The Lean Start'
+          price={11.96}
+          rating={3}
+          image='https://images-na.ssl-images-amazon.com/images/G/01/amazonglobal/images/email/asins/DURM-2447E617EC5C463L._V533746472_.jpg'
+        />
+                <Product
+          id='1323'
+          title='The Lean Start'
+          price={11.96}
+          rating={3}
+          image='https://images-na.ssl-images-amazon.com/images/G/01/amazonglobal/images/email/asins/DURM-2447E617EC5C463L._V533746472_.jpg'
+        />
+      </div>
+      <div className='home__row'>
+        <Product
+          id='1323'
+          title='The Lean Start'
+          price={11.96}
+          rating={3}
+          image='https://images-na.ssl-images-amazon.com/images/G/01/amazonglobal/images/email/asins/DURM-2447E617EC5C463L._V533746472_.jpg'
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
---
+++ src/Product.js
@@ -0,0 +1,30 @@
+import React from "react";
+import StarIcon from "@material-ui/icons/Star";
+import "./Product.css";
+
+function Product({ id, title, image, price, rating }) {
+  return (
+    <div className='product'>
+      <div className='product__info'>
+        <p>{title}</p>
+        <p className='product__price'>
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className='product__rating'>
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <p>
+                <StarIcon />
+              </p>
+            ))}
+        </div>
+      </div>
+      <img src={image} alt='' />
+      <button>Add To Basket</button>
+    </div>
+  );
+}
+
+export default Product;
